7665da3cb16e410cfaab5e546be6cc9f353e0221
Alexander Kuiper <kuiper9000@gmail.com>
2024-12-06 18:22:45 +0100

    added on mounted, update and delete funcions

--- src/stores/bookings.js
+++ src/stores/bookings.js
@@ -1,57 +1,60 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { jsonBinApi } from '@/services/jsonBinApi'
 import { v4 as uuIDv4 } from 'uuid'
 
 const bookingsURL = import.meta.env.VITE_JSONBIN_BIN_URL_BOOKINGS
 
 export const useBookingsStore = defineStore('bookings', () => {
-  // const bookings = ref([])
+  const bookings = ref(null)
   const isLoading = ref(false)
-  const bookings = ref([
-    {
-      id: 'B001',
-      item: 'I001',
-      renter: 'U002',
-      startDate: '2023-12-10',
-      endDate: '2023-12-15',
-    },
-    {
-      id: 'B002',
-      item: 'I002',
-      renter: 'U004',
-      startDate: '2023-12-20',
-      endDate: '2023-12-25',
-    },
-    {
-      id: 'B003',
-      item: 'I003',
-      renter: 'U001',
-      startDate: '2023-12-05',
-      endDate: '2023-12-10',
-    },
-    {
-      id: 'B004',
-      item: 'I004',
-      renter: 'U003',
-      startDate: '2024-01-01',
-      endDate: '2024-01-05',
-    },
-    {
-      id: 'B005',
-      item: 'I002',
-      renter: 'U004',
-      startDate: '2024-01-10',
-      endDate: '2024-01-15',
-    },
-    {
-      id: 'B006',
-      item: 'I001',
-      renter: 'U002',
-      startDate: '2024-01-20',
-      endDate: '2024-01-25',
-    },
-  ])
+
+  onMounted(fetchBookings)
+
+  // const bookings = ref([
+  //   {
+  //     id: 'B001',
+  //     item: 'I001',
+  //     renter: 'U002',
+  //     startDate: '2023-12-10',
+  //     endDate: '2023-12-15',
+  //   },
+  //   {
+  //     id: 'B002',
+  //     item: 'I002',
+  //     renter: 'U004',
+  //     startDate: '2023-12-20',
+  //     endDate: '2023-12-25',
+  //   },
+  //   {
+  //     id: 'B003',
+  //     item: 'I003',
+  //     renter: 'U001',
+  //     startDate: '2023-12-05',
+  //     endDate: '2023-12-10',
+  //   },
+  //   {
+  //     id: 'B004',
+  //     item: 'I004',
+  //     renter: 'U003',
+  //     startDate: '2024-01-01',
+  //     endDate: '2024-01-05',
+  //   },
+  //   {
+  //     id: 'B005',
+  //     item: 'I002',
+  //     renter: 'U004',
+  //     startDate: '2024-01-10',
+  //     endDate: '2024-01-15',
+  //   },
+  //   {
+  //     id: 'B006',
+  //     item: 'I001',
+  //     renter: 'U002',
+  //     startDate: '2024-01-20',
+  //     endDate: '2024-01-25',
+  //   },
+  // ])
 
   async function fetchBookings() {
     isLoading.value = true
@@ -82,11 +85,17 @@ export const useBookingsStore = defineStore('bookings', () => {
       bookings.value = await jsonBinApi.updateData(bookingsURL, updateArray)
     } catch (error) {
       console.error('Error in adding booking', error)
+    } finally {
+      isLoading.value = false
     }
   }
 
-  async function updateBooking() {
+  async function updateBooking(newBooking) {
     try {
+      bookings.value = await jsonBinApi.updateData(
+        bookingsURL,
+        bookings.value.map((booking) => (booking.id === newBooking.id ? newBooking : booking)),
+      )
     } catch (error) {
       console.error('Error updating booking:', error)
     }
@@ -94,10 +103,20 @@ export const useBookingsStore = defineStore('bookings', () => {
 
   async function deleteBooking(id) {
     try {
+      const deleteID = bookings.value.find((item) => item.id === id)
+      if (!deleteID) {
+        throw new Error('No item found')
+      }
+      bookings.value = await jsonBinApi.updateData(
+        bookingsURL,
+        bookings.value.filter((booking) => booking.id !== id),
+      )
     } catch (error) {
-      console.error('Error deleting booking:', error)
+      console.error('Error deleting booking:', error.message)
+    } finally {
+      isLoading.value = false
     }
   }
 
-  return { bookings, fetchBookings, addBooking }
+  return { bookings, fetchBookings, addBooking, deleteBooking, updateBooking, isLoading }
 })
