436608a2e70dfc388456c6001c3bc64b27769b28
Johan Söderlund <johan.soderlund96@gmail.com>
2024-12-06 11:41:09 +0100

    started on the inital items page template structure

--- src/views/ItemsView.vue
+++ src/views/ItemsView.vue
@@ -1,6 +1,23 @@
+<script setup></script>
+
 <template>
-  <h2>Items</h2>
-  <!-- search bar -->
-  <!-- grid with items to be rented so click to open to itemDetailView. The grid should dynamicly adjust to search bar  -->
-  <!-- each grid item should have name-price-owner-img -->
+  <div class="items-container">
+    <h2>Items test</h2>
+    <div class="items-grid">
+      <div v-for="item in items" :key="item.id" class="item-card">
+        <h3>{{ item.name }}</h3>
+        <img :src="item.img" alt="item.name" />
+        <div class="item-details">
+          <p><strong>Price:</strong> {{ item.price }}</p>
+          <p><strong>Owner:</strong> {{ item.owner }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped></style>
+
+<!-- search bar -->
+<!-- grid with items to be rented so click to open to itemDetailView. The grid should dynamicly adjust to search bar  -->
+<!-- each grid item should have name-price-owner-img -->
