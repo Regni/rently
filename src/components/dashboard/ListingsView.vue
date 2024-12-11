6cba9537ec1087605c8c834bff843f6974379e58
Anna-Sara Isakson <annasaraisakson@hotmail.com>
2024-12-11 11:00:48 +0100

    added dashboard, listings and bookings views

---
+++ src/components/dashboard/ListingsView.vue
@@ -0,0 +1,84 @@
+<script setup>
+import { useItemsStore } from '@/stores/items'
+
+const itemsStore = useItemsStore()
+const items = itemsStore.items
+</script>
+
+<template>
+  <section class="listings">
+    <h2>My Listings</h2>
+    <div class="container">
+      <div v-for="item in items" key="item.id">
+        <div class="item-card">
+          <img class="item-image" :src="item.images[0]" :alt="item.name" />
+          <div class="item-details">
+            <h3 class="item-title">{{ item.name }}</h3>
+            <p><strong>Price:</strong> {{ item.price }} kr</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button class="btn">+ Add New Listing</button>
+  </section>
+</template>
+
+<style scoped>
+.listings {
+  width: 75%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 48px;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+}
+
+h2 {
+  font-family: var(--font-headings);
+  margin: 1em;
+  color: var(--color-secondary);
+}
+
+.item-card {
+  background-color: var(--color-third);
+  border: 1px solid #ddd;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: space-between;
+  border-radius: 8px;
+  padding: 1.5rem;
+  width: auto;
+  height: auto;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  gap: 1em;
+}
+
+.item-card:hover {
+  opacity: 0.8;
+  cursor: pointer;
+}
+
+h3 {
+  font-family: var(--font-links);
+  color: var(--color-secondary);
+}
+
+p {
+  font-family: var(--font-basic);
+  color: var(--color-basic-text);
+}
+
+.btn {
+  font-size: 1.2em;
+  margin: 1em 0em 2em 0em;
+}
+</style>
---
+++ src/views/user/DashboardView.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { computed } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+
+// Computed property to determine which button text to display
+const buttonText = computed(() => {
+  return route.name === 'dashboard-listings' ? 'My Bookings' : 'My Listings'
+})
+
+// Function to toggle routes depending on the current route
+const toggleView = () => {
+  if (route.name === 'dashboard-listings') {
+    router.push({ name: 'dashboard-bookings' })
+  } else {
+    router.push({ name: 'dashboard-listings' })
+  }
+}
+</script>
+
+<template>
+  <section class="container">
+    <div class="side-bar">
+      <h2>User's Profile</h2>
+      <button class="btn" @click="toggleView">{{ buttonText }}</button>
+    </div>
+
+    <div class="vl"></div>
+    <RouterView />
+  </section>
+</template>
+
+<style scoped>
+.container {
+  max-width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: auto;
+}
+
+.side-bar {
+  width: 20%;
+  height: 90%;
+  align-self: flex-start;
+  padding: 3em 0;
+}
+
+h2 {
+  font-family: var(--font-headings);
+  color: var(--color-secondary);
+  margin: 30px;
+}
+
+.btn {
+  margin: 0 30px;
+}
+
+.vl {
+  border-left: 3px solid var(--color-secondary);
+  height: 80%;
+}
+</style>
